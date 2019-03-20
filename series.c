8f623a7a73cdcce94cd9bd4a6ec2b7e8bf518c40
Milan Shah <milanshah1@gmail.com>
2019-03-20 20:44:11 +0530

    fixes 2nd series

--- series.c
+++ series.c
@@ -9,13 +9,15 @@ void main(){
 		
 		printf("\nMenu\n1.Find sum of series '1 - x^2/2! + x^4/4! ...'\n2.Find sum of series '1 - x^1/1! + x^2/2! ...(fibonocci)'\nEnter Choice: ");
 		scanf("%d", &ch);
-		printf("Enter max power: ");
-		scanf("%d", &p);
-		printf("Enter value of x: ");
-		scanf("%d", &x);
+		
 		
 		switch(ch){
-			case 1:	sum = 0;
+			case 1: 
+					printf("Enter max power: ");
+					scanf("%d", &p);
+					printf("Enter value of x: ");
+					scanf("%d", &x);
+					sum = 0;
 					for(i = 2; i <= p; i+=2){
 						fac = i;
 						pow = x;
@@ -31,16 +33,24 @@ void main(){
 					}
 					printf("Sum is : %f", sum+1);
 					break;
-			case 2:	sum = 0;
+			case 2:	prev = 1, q = 1;
+					printf("Enter max terms: ");
+					scanf("%d", &p);
+					printf("Enter value of x: ");
+					scanf("%d", &x);
+					sum = 0;
 					mult = 1;
-					for(i = 0; i < p; i++){
+					for(i = 0; i < p-1; i++){
 						fac = q;
 						pow = x;
 						mult *= -1;
+						//printf(" [(%d)*%f/%f] ", mult, pow, fac);
 						for(j = 1; j < q; j++){
 							fac*=j;
 							pow*=x;
 						}
+						//printf(" ((%d)*%f/%f) ", mult, pow, fac);
+						//printf(" (%d, %d) ", q, prev);
 						sum += mult*pow/fac;
 						q = q + prev;
 						prev = q - prev;
@@ -51,8 +61,8 @@ void main(){
 		}
 		
 		printf("\nDo you want to continue? (y/n): ");
-		scanf("%c", &c);
-		if(c=='n'){
+		scanf("%s", &c);
+		if(c=='y'){
 			continue;
 		}
 		else{
