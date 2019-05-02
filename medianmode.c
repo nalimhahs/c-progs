471d8f56d2d5f63f361086a9d9949d1559592740
Milan Shah <milanshah1@gmail.com>
2019-05-02 12:12:53 +0530

    more mode stuff

--- medianmode.c
+++ medianmode.c
@@ -41,13 +41,21 @@ void multimode(int a[], int n)
 {
 	int cache[20], count = 0, j, i, k = 0, maxCount, m;
 
-	// for (j = 0; j < k; j++)
-	// {
-	// 	if (cache[j] == a[i])
-	// 	{
-	// 		goto cont;
-	// 	}
-	// }
+	cache[0] = mode(a, n);
+	maxCount = counter(a, n, cache[0]);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("\n%d\n%d\n", cache[0], maxCount);
+
+		if (a[i] == cache[0])
+		{
+			a[i] = -1;
+			break;
+		}
+	}
+	k++;
+
 
 	for (i = 0; i < n; i++)
 	{
@@ -55,7 +63,6 @@ void multimode(int a[], int n)
 		if (counter(a, n, m) == maxCount)
 		{
 			cache[k] = m;
-			k++;
 			for (i = 0; i < n; i++)
 			{
 				if (a[i] == cache[k])
@@ -64,15 +71,15 @@ void multimode(int a[], int n)
 					break;
 				}
 			}
+			k++;
 		}
-		//cont:
 	}
 
 	printf("\nMode is: ");
 
 	for(i = 0; i < k; i++)
 	{
-		printf("%d, ", cache[i]);
+		printf("%d , ", cache[i]);
 	}
 	
 }
